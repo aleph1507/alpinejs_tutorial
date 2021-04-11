b72d67f889ed433a1623e6f0f298270805aac2ce
Hristo <xrristo@gmail.com>
2021-04-11 20:06:29 +0200

    base files

---
+++ README.md
@@ -0,0 +1,13 @@
+Run `npm i`, `npm run start` to compile and start at [http://127.0.0.1:85](http://127.0.0.1:85)
+
+AlpineJS examples following this [tutorial](https://www.youtube.com/watch?v=VerLjLcXsTk&ab_channel=Scrimba).
+
+- Dropdown
+- Modal
+- Sidebar
+- Tabs
+- Scroll event and navbar
+- Accordion
+- Fetch, ajax calls
+- Todo app
+- Transitions, transformations
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+require('v8-compile-cache');
+
+module.exports = {
+    entry: './src/index.js',
+    watch: false,
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/img',
+                    to: 'img',
+                },
+                {
+                    from: 'src/index.html',
+                    to: ''
+                }
+            ],
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        watchContentBase: true,
+        port: 9000,
+    }
+}
