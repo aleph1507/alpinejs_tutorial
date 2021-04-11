fea9973f5cd1045e3f6d42c4fea2ef0667861f55
Hristo <xrristo@gmail.com>
2021-04-11 22:00:40 +0200

    gallery with external js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './style.css';
 import 'alpinejs';
+
+//gallery
+window.gallery = {
+    images: () => {
+        return [
+            {
+                src: '/img/france-french-alps-megeve.jpg',
+                alt: 'image 1'
+            },
+            {
+                src: '/img/French_Alps_banner.jpg',
+                alt: 'image 2'
+            },
+            {
+                src: '/img/josiahskeats-tmb3-ig-thumbnail-image-block.jpg',
+                alt: 'image 3'
+            },
+        ]
+    },
+
+    selectImage: ($event, displayed, index) => {
+        displayed.src = gallery.images()[index].src;
+        displayed.alt = gallery.images()[index].alt;
+        const thumbs = document.getElementsByClassName('thumbnail');
+        for (let i = 0; i<thumbs.length; i++) {
+            thumbs[i].classList.remove('border-green-400');
+        }
+        $event.target.parentNode.classList.add('border-green-400');
+    }
+};
